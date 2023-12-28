f8eecd096ba69612418e8147ea36c9bf7114abb9
Sylwia Budzynska <102833689+sylwia-budzynska@users.noreply.github.com>
2023-12-28 16:51:00 +0100

    Create app.py

---
+++ dbtest/app.py
@@ -0,0 +1,14 @@
+import gradio as gr
+
+
+def fn(text):
+        html = ("<div>" + text + "</div>")
+            return html
+
+
+        demo = gr.Interface(
+                    fn,
+                        inputs="text", outputs="html",
+                        )
+
+        demo.launch()
